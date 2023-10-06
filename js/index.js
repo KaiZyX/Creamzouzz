ac4cda77bc7e06ca191bc3c42f1fb3f3a72d2065
AnisDALI <anis.dali-ahmed@efrei.net>
2023-10-06 22:19:16 +0200

    Panier

--- js/index.js
+++ js/index.js
@@ -11,6 +11,41 @@ function smoothScroll(targetId) {
     }
 }
 
+// Fonction pour ajouter un article au panier
+function addToCart(productName, price) {
+    const cartItems = document.getElementById('cart-items');
+    const cartTotal = document.getElementById('cart-total');
+
+    // Crée un élément de liste pour l'article
+    const item = document.createElement('li');
+    item.textContent = productName + ' - ' + price + ' €';
+    
+    // Ajoute l'article à la liste du panier
+    cartItems.appendChild(item);
+
+    // Met à jour le total du panier
+    const currentTotal = parseFloat(cartTotal.textContent);
+    cartTotal.textContent = (currentTotal + price).toFixed(2); // Met en forme le total avec 2 décimales
+}
+
+// Gestionnaire d'événement pour les boutons "Ajouter au panier"
+const addToCartButtons = document.querySelectorAll('.addToCart');
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const productName = button.getAttribute('data-product');
+        const price = parseFloat(button.getAttribute('data-price'));
+        addToCart(productName, price);
+    });
+});
+
+// Gestionnaire d'événement pour le bouton "Valider la commande"
+const checkoutButton = document.getElementById('checkout-button');
+checkoutButton.addEventListener('click', () => {
+    // Ici, vous pouvez implémenter le processus de paiement, par exemple, en redirigeant l'utilisateur vers une page de paiement.
+    // Vous devrez également envoyer les détails du panier au serveur pour finaliser la commande.
+});
+
+
 // Ajoutez des gestionnaires d'événements pour les liens de navigation
 const navLinks = document.querySelectorAll(".nav-link");
 navLinks.forEach(link => {
